05fe6a5def159ed62c2117898e4c6dc38dfb4dc3
Jacqueline Deans <jdeans289@gmail.com>
2023-06-08 11:51:40 -0500

    Add more exercises

--- c_exercises.cpp
+++ c_exercises.cpp
@@ -194,7 +194,7 @@ TEST (C_Exercise, dynamic_allocation1) {
 
 TEST (C_Exercise, dynamic_allocation2) {
 
-    char ** sentence;
+    const char ** sentence;
 
     /********************* MAKE YOUR CHANGES HERE *************************/
     FAIL() << TEST_NAME  << " is not yet implemented" ;
@@ -202,6 +202,10 @@ TEST (C_Exercise, dynamic_allocation2) {
     // Allocate space for two char pointers sentence
     // set the first value to "Hello"
     // set the second value to "World"
+
+    // Note that string literals are considered type "const char *"
+    // Meaning that the pointer is read-only
+    // Be careful to use const in the correct way to avoid compiler warnings and errors!
 
     /**********************************************************************/
 
@@ -253,5 +257,116 @@ TEST (C_Exercise, preprocessor) {
     FAIL() << "Use preprocessor definitions to exclude this line" ;
     #endif
 }
+
+struct MyStruct {
+    int a; 
+    const char * b;
+    double c[3];
+};
+
+TEST (C_Exercise, struct) {
+
+    /********************* MAKE YOUR CHANGES HERE *************************/
+    // Remove these two lines when you are ready to test
+    FAIL() << TEST_NAME  << " is not yet implemented" ;
+    #define NOT_IMPLEMENTED
+
+    // Declare an instance of MyStruct named obj
+    // Set a to 5
+    // Set b to "Hello"
+    // Set c to {0.1, 0.2, 0.3}
+
+    /**********************************************************************/
+
+    #ifndef NOT_IMPLEMENTED
+    ASSERT_EQ(obj.a, 5);
+    ASSERT_STREQ(obj.b, "Hello");
+    ASSERT_EQ(obj.c[0], 0.1); 
+    ASSERT_EQ(obj.c[1], 0.2); 
+    ASSERT_EQ(obj.c[2], 0.3); 
+    #endif
+}
+
+TEST (C_Exercise, struct_pointer) {
+
+    /********************* MAKE YOUR CHANGES HERE *************************/
+    FAIL() << TEST_NAME  << " is not yet implemented" ;
+
+    struct MyStruct * obj_ptr;
+
+    // Allocate obj_ptr
+    // Set a to 5
+    // Set b to "Hello"
+    // Set c to {0.1, 0.2, 0.3}
+
+    /**********************************************************************/
+
+    ASSERT_EQ(obj_ptr->a, 5);
+    ASSERT_STREQ(obj_ptr->b, "Hello");
+    ASSERT_EQ(obj_ptr->c[0], 0.1); 
+    ASSERT_EQ(obj_ptr->c[1], 0.2); 
+    ASSERT_EQ(obj_ptr->c[2], 0.3); 
+}
+
+TEST (C_Exercise, enums) {
+
+    // Remove this line when you've implemented this test
+    FAIL() << TEST_NAME  << " is not yet implemented" ;
+
+    // Use enums to create a set options for something
+    enum Color {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, PURPLE};
+    Color my_color = BLUE;
+
+    // Enums work well with switch statments
+    // Fill in this switch statment to set the value of my_color_name
+    const char * my_color_name;
+
+    switch (my_color) {
+        case RED:
+            my_color_name = "red";
+            break;
+    /********************* MAKE YOUR CHANGES HERE *************************/
+        // Fill in the rest of the cases
+        // Don't forget to break!
+    /**********************************************************************/
+        default:
+            my_color_name = "This is not a color!";
+    }
+
+    printf("My color is: %s\n", my_color_name);
+    ASSERT_STREQ(my_color_name, "blue");
+}
+
+struct Rectangle {
+    int height;
+    int width;
+};
+
+int getArea (struct Rectangle * r) {
+    /********************* MAKE YOUR CHANGES HERE *************************/
+    // Calculate the area of the rectangle
+    return -1;
+    /**********************************************************************/
+}
+
+TEST (C_Exercise, area) {
+
+    // This is special syntax to initialize the members in the struct
+    struct Rectangle my_rectangle = { .height = 5, .width = 2};
+    int area;
+
+    /********************* MAKE YOUR CHANGES HERE *************************/
+    FAIL() << TEST_NAME  << " is not yet implemented" ;
+
+    // Call the getArea function with my_rectangle - note that getArea takes a pointer to a rectangle
+    // Set 'area' to the result
+    /**********************************************************************/
+
+    EXPECT_EQ(area, 10);
+}
+
+
+
+
 
 
